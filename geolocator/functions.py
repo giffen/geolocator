2496e089bc395ff1cb0c17c94e8893425a6b3886
Andy Giffen <giffen@gmail.com>
2014-04-23 11:38:13 -0700

    Add foursquare search

--- geolocator/functions.py
+++ geolocator/functions.py
@@ -1,12 +1,18 @@
 import urllib2
 import json
+from geopy.geocoders import GoogleV3
 
 from apis import *
 
-api = locu_api
-url = "https://api.locu.com/v1_0/venue/search/?"
+def find_place(query):
+	g = GoogleV3()
+	place, (lat, lng) = g.geocode(query)
+	return place, lat, lng
 
 def locu_search(query):
+	api = locu_api
+	url = "https://api.locu.com/v1_0/venue/search/?"
+
 	local = query
 	locality = local.replace(' ','%20')
 
@@ -20,4 +26,24 @@ def locu_search(query):
 	for abc in data['objects']:
 		locations.append(abc['name'])
 
+	return locations
+
+def foursquare_search(query):
+	token = foresquare_token
+
+	place, lat, lng = find_place(query)
+	latlng = str(lat) + "%2C" + str(lng)
+	
+	url = "https://api.foursquare.com/v2/venues/search?v=20131016&ll=" + latlng + "&intent=checkin&oauth_token="
+	full_url = url + token
+
+	obj = urllib2.urlopen(full_url)
+
+	data = json.load(obj)
+
+	locations = []
+
+	for abc in data['response']['venues']:
+		locations.append(abc['name'])
+		
 	return locations
--- geolocator/views.py
+++ geolocator/views.py
@@ -1,12 +1,13 @@
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, render_to_response, RequestContext, get_object_or_404
 
-from .functions import locu_search
+from .functions import locu_search, foursquare_search
 
 def home(request):
 	if request.method == 'POST':
 		q = request.POST['search']
-		locations = locu_search(q)
+		#locations = locu_search(q)
+		locations = foursquare_search(q)
 		print locations
 
 	return render_to_response('home.html', locals(), context_instance=RequestContext(request))
